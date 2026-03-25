791c629d16a899677ab6fb58ec59e309fe8f02db
Rasztovich Daniel <daniel.rasztovich@activesolution.at>
2026-03-25 15:56:11 +0100

    Initial commit

---
+++ ecommerce-core/src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,25 @@
+package com.luv2code.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "product_category")
+public class ProductCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+
+}
