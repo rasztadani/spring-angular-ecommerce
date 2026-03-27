d185ac3ff7b21cdc0771e72d553f654dd04ea170
rasztadani <rasztadani@gmail.com>
2026-03-27 09:10:24 +0100

    any changes

--- ecommerce-gui/src/app/app.config.ts
+++ ecommerce-gui/src/app/app.config.ts
@@ -1,23 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+} from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
-import { productsReducer } from './store/products/products.reducer';
-import { provideEffects } from '@ngrx/effects';
-import { ProductsEffects } from './store/products/products.effects';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { routes } from "./app.routes";
+import { provideStore } from "@ngrx/store";
+import { productsReducer } from "./store/products/products.reducer";
+import { provideEffects } from "@ngrx/effects";
+import { ProductsEffects } from "./store/products/products.effects";
+import { provideStoreDevtools } from "@ngrx/store-devtools";
+import { provideHttpClient } from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideStore({
       products: productsReducer,
     }),
     provideEffects([ProductsEffects]),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideStoreDevtools(
-      { maxAge: 25, logOnly: false },
-    )
-  ]
+    provideStoreDevtools({ maxAge: 25, logOnly: false }),
+  ],
 };
--- ecommerce-gui/src/app/app.ts
+++ ecommerce-gui/src/app/app.ts
@@ -1,19 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { ProductListComponent } from './components/product-list/product-list';
-import { ProductCategoryMenu } from './components/product-category-menu/product-category-menu';
-import { SearchComponent } from './components/search/search';
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { ProductCategoryMenu } from "./components/product-category-menu/product-category-menu";
+import { SearchComponent } from "./components/search/search";
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    ProductCategoryMenu,
-    SearchComponent,
-  ],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  selector: "app-root",
+  imports: [RouterOutlet, ProductCategoryMenu, SearchComponent],
+  templateUrl: "./app.html",
+  styleUrl: "./app.css",
 })
 export class App {
-  protected readonly title = signal('angular-ecommerce');
+  protected readonly title = signal("ecommerce-gui");
 }
